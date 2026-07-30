528ff1e19289f5385aaf2700c5e7cbe735dd477f
Ray <Ray@Rays-iMac.local>
2026-07-30 13:52:59 +0900

    Add Cloudflare Web Analytics

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "market-dashboard-v136";
+const CACHE_VERSION = "market-dashboard-v137";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 
