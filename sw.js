23e1b336694c14607ffaa4f60e3fc96db0357c17
Ray <Ray@Rays-iMac.local>
2026-08-22 22:09:02 +0900

    Update default FX series

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = "market-dashboard-v183";
+const CACHE_VERSION = "market-dashboard-v184";
 const APP_SHELL_CACHE = `${CACHE_VERSION}-shell`;
 const DATA_CACHE = `${CACHE_VERSION}-data`;
 
@@ -7,7 +7,7 @@ const APP_SHELL_ASSETS = [
   "./index.html",
   "./style.css?v=indicator-expansion-v62",
   "./ui-template.css?v=ui-template-v25",
-  "./app.js?v=indicator-expansion-v114",
+  "./app.js?v=indicator-expansion-v115",
   "./manifest.json",
   "./offline.html",
   "./icons/icon-192.png?v=2",
